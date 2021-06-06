6fe67842a73d09726ef1d5ea5e2ea8a1e1052a13
Futa HIRAKOBA <futa-hirakoba@cybozu.co.jp>
2021-06-06 17:18:34 +0900

    feat: enable ssh

--- var.tf
+++ var.tf
@@ -11,14 +11,28 @@ variable "name" {
 }
 
 variable "vcn_cidr_block" {
-  default = "192.168.100.0/24"
+  default = "10.0.0.0/16"
 }
 
 variable "subnet_cidr_block" {
-  default = "192.168.100.0/27"
+  default = "10.0.0.0/24"
 }
 
 variable "instance_image_ocid_ubuntu2004" {
   # https://docs.oracle.com/en-us/iaas/images/image/4d2aaffa-325f-4fe7-9e99-a5b1ee7f2cd1/
   default = "ocid1.image.oc1.ap-osaka-1.aaaaaaaa4opxgypfsfz7pu2hqka47tdynl3vgoy5ejckofqkygkxj2xnjijq"
+}
+
+variable "ssh_public_key" {
+  default = ""
+}
+
+variable "nsg_source_cidr" {
+  description = "Allowed Ingress Traffic (CIDR Block)"
+  default     = "0.0.0.0/0"
+}
+
+variable "nsg_ssh_port" {
+  description = "SSH Port"
+  default     = 22
 }
